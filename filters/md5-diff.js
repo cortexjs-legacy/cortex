a8f9e4155cf88a79bda65b4b913daa38647e5232
Kael <i@kael.me>
2012-10-29 16:11:32 +0800

    filters: add filters: md5.diff and publish-imitate

--- filters/md5-diff.js
+++ filters/md5-diff.js
@@ -7,8 +7,11 @@
 var
 
 TEMP_DIR = '.cortex',
+SUCCESS_LOCK_FILE = 'success.lock',
+MD5_FILE = 'md5.json',
 
 fs = require('fs'),
+fs_more = require('../util/fs-more'),
 path = require('path');
 
 
@@ -17,8 +20,10 @@ path = require('path');
     project_root: {string}
  }
  */
-function Diff(params){
-    this.project_root = params.project_root;
+function Diff(options){
+    this.cwd = options.cwd;
+    
+    this._getConfig();
     this._prepareData(params);
 };
 
@@ -32,11 +37,36 @@ Diff.prototype = {
      * @param {function()} callback will be executed when completed
      */
     run: function(callback){
+        var
         
-    },
-    
-    tearDown: function(){
+        pathname;
+        cur_md5, last_md5;
         
+        try{
+            pathname = path.join(this.cur_build_root, TEMP_DIR, MD5_FILE);
+            cur_md5 = JSON.parse( fs.readFileSync(pathname) );
+        
+        }catch(e){
+            console.log(e);
+            throw 'error parsing: ' + pathname;
+        }
+        
+        if(this.last_build_root){
+            try{
+                pathname = path.join(this.last_build_root, TEMP_DIR, MD5_FILE);
+                last_md5 = JSON.parse( fs.readFileSync(pathname) );
+            
+            }catch(e){
+                console.log(e);
+                throw 'error parsing: ' + pathname;
+            }
+        }
+        
+        var
+        
+        diff = this._diff(cur_md5, last_md5 || {});
+        
+        this._writeList(diff);
     },
     
     _diff: function(list, rel_list){
@@ -56,21 +86,81 @@ Diff.prototype = {
         var
         
         fd = fs.openSync( path.join(this.cur_build_root, TEMP_DIR );
-        
         fs.writeSync(fd, JSON.stringify(list));
-        
         fs.closeSync(fd);
     },
     
     _prepareData: function(params){
+        this._getBuildRoot();
+    
         var
         
-        cur_build_root = params.
+        build_dirs = this._getSortedBuildDirs();
+        
+        if(!build_dirs.length){
+            throw 'no build directory found, there should be a directory like: build-<timestamp>'
+        }
+        
+        
+        this.cur_build_root = path.join(this.cwd, this.build_root, build_dirs.shift());
+        
+        var 
+        i = 0,
+        len = build_dirs.length,
+        dir,
+        last_build_dir;
+        
+        for(; i < len; i ++){
+            dir = build_dirs[i];
+            if(
+                // there must be a 'success.lock' file at the last successfull build directory
+                fs_more.isFile(
+                    path.join(this.cwd, this.build_root, dir, TEMP_DIR, SUCCESS_LOCK_FILE)
+                )
+            ){
+                last_build_dir = dir;
+                break;
+            }
+        }
+        
+        this.last_build_root = !!last_build_dir ? path.join(this.cwd, this.build_root, last_build_dir) : false;
+        
+    },
+    
+    _getBuildRoot: function(){
+        return this.build_root = path.join(this.cwd, this.config.build_directory || 'build');
+    },
+    
+    _getSortedBuildDirs: function(){
+        var 
+        
+        root = this.build_root,
+        dir_content = fs.readdirSync(root);
+    
+        return dir_content.filter(function(current){
+            return fs_more.isDirectory(path.join(root, current));
+            
+        }).sort(function(a, b){
+            return a < b;
+        });
+    },
+        
+    _getConfig: function(){
+        var
+        
+        content = fs.readFileSync(path.join(this.cwd, CONFIG_FILE)),
+        config;
+        
+        try{
+            config = JSON.parse(content);
+        }catch(e){
+            console.log('error info:', e);
+            throw 'parsing publish.json failed, please check your code.';
+        }
+        
+        return this.config = config;
     }
 };
-
-
-
 
 
 module.exports = {
