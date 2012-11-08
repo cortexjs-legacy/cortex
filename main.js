bfc7553e55f5d84e78c9a3f149cf7fe32b4bc488
supersheep <supersheepxjy@gmail.com>
2012-11-08 18:03:36 +0800

    add filter option for package action

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ var tracer = require("tracer").colorConsole(),
 
 
 // 主流程
-function main(root){
-
+function main(root,opts){
+    var filter = opts.filter || [];
     console.log("开始应用已配置的滤镜 >>>>>>>>>>");
 
          
@@ -18,6 +18,7 @@ function main(root){
         cwd: root
     };
     
+
     [
         'update',
         'publish-imitate',
@@ -26,7 +27,6 @@ function main(root){
         'closure',
         'md5',
         'md5-diff'
-    
     ].forEach(function(filter){
         filterEngine.assign(filter, options);
     });
