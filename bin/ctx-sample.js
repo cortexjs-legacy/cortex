113831f928d6c6e7354c03acdc83f630fed64d7f
Wei Gao <jky239@gmail.com>
2013-05-08 16:32:22 +0800

    wrapping to prototype

--- bin/ctx-sample.js
+++ bin/ctx-sample.js
@@ -1,17 +1,18 @@
 #! /usr/bin/env node
 
 // sub command, just like a standalone program
-var program = require("commander");
+var program = require("commander")
+    , tracer = require("tracer").colorConsole();
 
 if (require.main) {
     program.option("-p, --program <program>", "")
         .parse(process.argv);
 
     if (program.program !== undefined) console.log("-p argument:", program.program);
-
     var npm = require('../lib/npmw.js');
 
-    npm.load(function(er, npm) {
+    npm.load(function(er) {
+        console.log(npm.commands);
         console.log(npm.config.get("registry"));
     });
 
