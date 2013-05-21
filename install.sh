86e4b9ebac24c1bd29ab82f529921133f365f42b
Kael <i@kael.me>
2013-05-21 14:21:08 +0800

    remove unnecessary files

--- install.sh
+++
@@ -1,9 +0,0 @@
-npm install 
-wget -O gifsicle-1.68.tar.gz http://www.lcdf.org/gifsicle/gifsicle-1.68.tar.gz
-tar -zxvf gifsicle-1.68.tar.gz
-cd gifsicle-1.68
-./configure
-make
-make install
-cd ..
-rm -rf gifsicle-1.68.tar.gz gifsicle-1.68
