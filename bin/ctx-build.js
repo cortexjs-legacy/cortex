0bf6954055fa65e6d1ca4eb77d6d60cf60d8eb3e
Kael <i@kael.me>
2013-05-27 17:11:43 +0800

    update build action

--- bin/ctx-build.js
+++ bin/ctx-build.js
@@ -1,7 +1,31 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+var nopt = require('nopt');
+var node_path = require('path');
+
+var build = require('../lib/build.js');
 
 
-var grunt = require('grunt');
+var option_list = {
+	cwd: {
+		short: 'c',
+		info: 'specify folder to build',
+		type: node_path
+	}
+};
+
+var known_opts = {};
+var short_hands = {};
+
+var parsed = nopt(known_opts, short_hands, process.argv, 2);
+var argv = parsed.argv;
+delete parsed.argv;
+
+build(parsed);
+
+
+
+
+
+
 
