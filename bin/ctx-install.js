6de41b7c20c1d8cbd5c1c5722f76ca4a841a965b
Kael <i@kael.me>
2013-05-28 18:19:41 +0800

    UNTESTED! blind coded `ctx install`

--- bin/ctx-install.js
+++ bin/ctx-install.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 var node_path = require('path');
-var option = require('../lib/option.js');
-var fs = require('fs-more').sync;
-var install = require('cortex-install');
+var async = require('async');
+var fs = require('fs-sync');
+var install = require('cortex-install').install;
+
+var grunt_runner = require('../lib/run-grunt');
+var option = require('../lib/option');
 
 var option_list = {
 	cwd: {
@@ -24,7 +27,6 @@ var modules = parsed.argv.remain;
 var has_package = fs.exists('package.json');
 
 if(modules.length === 0){
-
 	if(has_package){
 		var pkg = fs.readJSON('package.json');
 		var deps = modules = (pkg.cortexDependencies || {});
@@ -40,6 +42,93 @@ if(modules.length === 0){
 	}
 }
 
+
+var USER_HOME = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+var CORTEX_ROOT = node_path.join(USER_HOME, '.cortex');
+
+var temp_dir = node_path.join(CORTEX_ROOT, 'tmp', + new Date);
+var module_root = node_path.join(USER_HOME, '.cortex', 'modules');
+
+var series = [];
+
+// {
+//		a: ['1.2.3', '1.3.3'],
+// 		b: ['0.0.1']
+// }
+var installed_module_map = {};
+
+
+function log(msg){
+	process.stdout.write(msg + '\n');
+};
+
+
+modules.forEach(function(module){
+	series.push(function(done) {
+
+		log();
+
+	    install(temp_dir, module, function(error, data) {
+	        if(error){
+	        	throw error;
+	        }
+
+	        var dependencies = data.dependencies;
+	        var key;
+
+	        for(module_name in dependencies){
+	        	if( !(module_name in installed_module_map) ){
+
+	        		// there might be more than one version for a certain module
+	        		installed_module_map[module_name] = [];
+	        	}
+
+
+	        	installed_module_map[module_name].push(dependencies[dependencies]);
+	        }
+
+	        done();
+	    });
+	});
+});
+
+
+async.parallel(series, function() {
+	var installed_modules = [];
+	var series = [];
+
+	Object.keys(installed_module_map).forEach(function(module) {
+		var versions = installed_module_map[module];
+
+		installed_modules = installed_modules.concat( versions.map(function(version) {
+		    return node_path.join(module, version);
+		}) );
+	});
+
+	installed_modules.forEach(function(module_slash_version) {
+	    series.push(function(done) {
+
+	    	log('run grunt-task: cortex.build on ', node_path.join(temp_dir, module_slash_version));
+	        grunt_runner('cortex.build', {
+	        	cwd: node_path.join(temp_dir, module_slash_version)
+
+	        }, done);
+	    })
+	});
+
+	async.parallel(series, function() {
+
+		installed_modules.forEach(function(module_slash_version) {
+		    fs.copy(
+		    	node_path.join(temp_dir, module_slash_version),
+		    	node_path.join(module_root, module_slash_version), {
+		    		force: true
+		    	}
+		    );
+		});
+	    
+	});
+});
 
 
 
