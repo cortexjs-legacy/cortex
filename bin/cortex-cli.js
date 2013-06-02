3cd1737441c98148932d4c0d2270728988447890
Kael <i@kael.me>
2013-06-02 20:20:36 +0800

    bin: moving all programming logic out of bin/

--- bin/cortex-cli.js
+++ bin/cortex-cli.js
@@ -2,58 +2,13 @@
 
 'use strict';
 
-var cortex = require('../');
-var fs = require('fs-sync');
 var node_path = require('path');
-var commander = require('../lib/util/commander');
+var parser = require('../lib/util/parse-argv');
 
+// 
 var argv = process.argv;
-
-var option_list = {
-	help: {
-		short: 'h',
-		type: Boolean
-	},
-
-	list: {
-		short: 'l',
-		type: Boolean
-	}
-};
-
-
-var parsed = commander.parse(option_list, argv, 2);
-
-var command;
-var config = {};
-
-// ctx --help
-// ctx -h
-// ctx
-if(parsed.help || argv.length === 2){
-	command = 'help';
-
-// ctx --help --list
-// ctx --list
-// ctx -l
-}else if(parsed.list){
-	command = 'help';
-	config.list = true;
-
-// ctx <command>
-}else{
-	command = argv[2];
-}
-
-var lib_root = node_path.join( __dirname, '..', 'lib');
-var command_root = node_path.join( lib_root, 'command' );
-var opt_root = node_path.join( lib_root, 'opt' );
-
-if ( !fs.exists( cli_root, command + '.js' ) ) {
-    process.stdout.write('Command <ctx ' + command + '> not found. See "ctx --help".\n');
-    process.exit(1);
-}
+var parsed = parser(argv);
 
 // run command
-require( node_path.join( cli_root, command ) ).run(config);
+require( node_path.join( __dirname, '..', 'lib', 'command', parsed.command ) )( parsed.options );
 
