a5deac1623ffd5b9e631c6d2e9358fcbd63ec494
Kael <i@kael.me>
2013-06-05 09:33:28 +0800

    write init value to \~/.cortex/stable_modules to make sure fs.readJSON wont fail

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ git clone git@github.com:supersheep/grunt-init-neuron.git ~/.grunt-init/neuron
 mkdir -p ~/.cortex/built_modules
 mkdir -p ~/.cortex/modules
 touch ~/.cortex/stable_modules
+echo "{}" >> ~/.cortex/stable_modules
 
 echo "Cortex successfully installed!"
 
