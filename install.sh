92dc1b9165aa5036ba2b86f1dea60f37a21c44b7
Kael <i@kael.me>
2013-06-07 11:53:27 +0800

    improve install.sh; avoid json error

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ git clone git@github.com:supersheep/grunt-init-neuron.git ~/.grunt-init/neuron
 mkdir -p ~/.cortex/built_modules
 mkdir -p ~/.cortex/modules
 touch ~/.cortex/stable_modules
+rm ~/.cortex/stable_modules
 echo "{}" >> ~/.cortex/stable_modules
-
 echo "Cortex successfully installed!"
 
 
