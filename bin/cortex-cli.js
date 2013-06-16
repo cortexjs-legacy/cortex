aa317f0a1cbcd407b51c3abc4b307fe862ff2c74
Kael <i@kael.me>
2013-06-16 12:25:48 +0800

    cache commander instance and explode to cortex exports

--- bin/cortex-cli.js
+++ bin/cortex-cli.js
@@ -2,12 +2,4 @@
 
 'use strict';
 
-var node_path = require('path');
-var commander = require('sub-commander');
-
-commander({
-	commands: node_path.join( __dirname, '..', 'lib', 'command'),
-	options : node_path.join( __dirname, '..', 'lib', 'option'),
-	name: 'ctx'
-}).cli();
-
+require('../commander').cli();
