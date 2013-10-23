d3002b9a1849d31ec52746917f41d39195fc05e5
kaelzhang <i@kael.me>
2013-10-23 20:31:27 +0800

    profile: adjust output; related to #74

--- bin/setting/profile.js
+++ bin/setting/profile.js
@@ -5,6 +5,6 @@ var profile = require('../../lib/profile');
 module.exports = profile().on('error', function (err) {
     process.stdout.write('\u001b[41m\u001b[37mWARNING\u001b[0m ' + err.message + ':\n');
     process.stdout.write('  - fix the file manually: "cortex config -e"\n');
-    process.stdout.write('  - reset the configuration file: "cortex config --unset-all".\n\n');
+    process.stdout.write('  - or reset it: "cortex config --unset-all".\n\n');
 
 }).init();
