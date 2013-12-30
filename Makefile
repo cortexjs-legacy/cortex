e51fdd0b05c4207ba7551b3a79e4e5574fde79e5
kaelzhang <i@kael.me>
2013-12-30 18:03:59 +0800

    #216: add test files;

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ REPORTER = spec
 test:
 		@./node_modules/.bin/mocha \
 			--reporter $(REPORTER) \
-			./test/*
+			./test/spec/**/*.js
 
 .PHONY: test
---
+++ test/spec/util/package.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var pkg = require('../../../lib/util/package');
+var expect = require('chai').expect;
+
+describe("", function(){
+    // code ...
+});
