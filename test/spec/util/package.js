06c086d02a6e11b9616be67628326a1f7106b832
kaelzhang <i@kael.me>
2014-01-09 17:22:56 +0800

    #225: update test cases; use temp dirs to play with test cases

--- test/spec/util/package.js
+++ test/spec/util/package.js
@@ -1,12 +1,37 @@
 'use strict';
 
 var pkg = require('../../../lib/util/package');
+var tmp = require('../../lib/tmp');
+
 var expect = require('chai').expect;
-var fs = require('fs');
+var fs = require('fs-sync');
 
 var node_path = require('path');
 
-var root = node_path.resolve('test', 'fixtures', 'util');
+var root = node_path.resolve('test', 'playground', 'tmp');
+var fixtures = node_path.resolve('test', 'fixtures');
+
+var only_package    = make(['package.json']);
+var hybrid          = make(['package.json', 'cortex.json']);
+var non_existing    = make([]);
+
+
+function make (files) {
+    var dir = tmp.make(root);
+
+    files.forEach(function (file) {
+        copy(file, fixtures, dir);
+    });
+
+    return dir;
+}
+
+function copy (file, from, to) {
+    fs.copy( node_path.join(from, file), node_path.join(to, file), {
+        force: true
+    });
+}
+
 
 describe("pkg.get_package_file(cwd)", function(){
     it("pkg.is_cortex_json", function(){
@@ -15,9 +40,7 @@ describe("pkg.get_package_file(cwd)", function(){
     });
 
     it("returns package.json if cortex.json is not existed", function(done){
-        var cwd = node_path.join(root, 'only-package');
-
-        pkg.get_package_file(cwd, function (err, file) {
+        pkg.get_package_file(only_package, function (err, file) {
             expect(err).to.equal(null);
             expect( pkg.is_cortex_json(file) ).to.equal(false);
             
@@ -26,9 +49,7 @@ describe("pkg.get_package_file(cwd)", function(){
     });
 
     it("returns cortex.json if existed", function(done){
-        var cwd = node_path.join(root, 'hybrid');
-
-        pkg.get_package_file(cwd, function (err, file) {
+        pkg.get_package_file(hybrid, function (err, file) {
             expect(err).to.equal(null);
             expect(pkg.is_cortex_json(file)).to.equal(true);
             done();
@@ -36,18 +57,14 @@ describe("pkg.get_package_file(cwd)", function(){
     });
 
     it("throws error if neither is existed, strict=true", function(done){
-        var cwd = node_path.join(root, 'non-existing');
-
-        pkg.get_package_file(cwd, function (err, file) {
+        pkg.get_package_file(non_existing, function (err, file) {
             expect(err).not.to.equal(null);
             done();
         }, true);
     });
 
     it("won't throw error if neither is existed, strict=", function(done){
-        var cwd = node_path.join(root, 'non-existing');
-
-        pkg.get_package_file(cwd, function (err, file) {
+        pkg.get_package_file(non_existing, function (err, file) {
             expect(err).to.equal(null);
             done();
         });
@@ -57,18 +74,14 @@ describe("pkg.get_package_file(cwd)", function(){
 
 describe("pkg.get_original_package(cwd)", function(){
     it("merges normal fields", function(done){
-        var cwd = node_path.join(root, 'only-package');
-
-        pkg.get_original_package(cwd, function (err, pkg) {
+        pkg.get_original_package(only_package, function (err, pkg) {
             expect(pkg.name).to.equal('foo');
             done();
         });
     });
 
     it("prevents merging special fields", function(done){
-        var cwd = node_path.join(root, 'only-package');
-
-        pkg.get_original_package(cwd, function (err, pkg) {
+        pkg.get_original_package(only_package, function (err, pkg) {
             expect(pkg.scripts.prebuild).to.equal(undefined);
             expect(pkg.dependencies.baar).to.equal(undefined);
             expect(pkg.dependencies.bar).not.to.equal(undefined);
@@ -77,12 +90,13 @@ describe("pkg.get_original_package(cwd)", function(){
     });
 
     it("use_inherits=true", function(done){
-        var cwd = node_path.join(root, 'only-package');
-
-        pkg.get_original_package(cwd, function (err, pkg) {
+        pkg.get_original_package(only_package, function (err, pkg) {
             expect(pkg.hasOwnProperty('name')).to.equal(false);
             done();
         }, true);
     });
 });
+
+
+
 
