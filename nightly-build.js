3dd176fe7ccf81afb092df0900ec32192b4b9e11
kaelzhang <i@kael.me>
2014-06-10 15:25:42 +0800

    nightly-build: changes bin from "cortex" -> "cortex-canary"

--- nightly-build.js
+++ nightly-build.js
@@ -5,17 +5,19 @@ var jf = require('jsonfile');
 
 var pkg = jf.readFileSync('package.json');
 
-function changeName (name) {
+function changeName (name, old) {
   pkg.name = name;
+  pkg.bin[name] = pkg.bin[old];
+  delete pkg.bin[old];
   jf.writeFileSync('package.json', pkg);
   console.log('name changed to', name);
 }
 
 process.on('exit', function () {
-  changeName('cortex');
+  changeName('cortex', 'cortex-canary');
 });
 
-changeName('cortex-canary');
+changeName('cortex-canary', 'cortex');
 
 spawns('npm publish', {
   cwd: __dirname,
