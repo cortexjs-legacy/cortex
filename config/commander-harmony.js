f025f01a8f94daee1d9adf1ffcc86d03a28f77cf
kaelzhang <i@kael.me>
2014-06-26 21:20:11 +0800

    config: adds harmony commander, update all services; #405

---
+++ config/commander-harmony.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var node_path = require('path');
+var comfort   = require('comfort');
+var logger    = require('./logger');
+var handler   = require('cortex-command-errors');
+var profile   = require('./profile');
+
+var context = {
+  profile: profile,
+  neuropil: require('./neuropil'),
+  locale: require('./i18n'),
+  logger: logger
+};
+
+var root = node_path.join(__dirname, '..');
+
+// Commander for CLI
+// cli entrance
+// cache commander instance
+var commander = module.exports = comfort({
+  command_root: node_path.join(root, 'lib', 'command'),
+  option_root: node_path.join(root, 'lib', 'option'),
+  root: root,
+  name: 'cortex',
+  context: context
+})
+.on('error', handler({
+  logger: logger,
+  harmony: true
+}));
+
+context.commander = commander;
