e933949b26303898e1ebb200d98fa16774426cda
kaelzhang <i@kael.me>
2014-08-18 15:34:00 +0800

    updates link.sh; 6.0.4 -> just republish

--- link.sh
+++ link.sh
@@ -4,3 +4,6 @@ __dirname=`pwd`
 
 sudo ln -sf "$__dirname/bin/cortex-cli.js" "/usr/local/bin/cortex"
 echo "$__dirname/bin/cortex-cli.js -> /usr/local/bin/cortex"
+
+sudo ln -sf "$__dirname" "/usr/local/lib/node_modules/cortex"
+echo "$__dirname -> /usr/local/lib/node_modules/cortex"
